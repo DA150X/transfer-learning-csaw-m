36ee4e94de06bc9e221dc9bdf84ac82d22d844f5
Noa Zetterman <noaz@kth.se>
2022-04-14 13:52:42 +0200

    Refactored and added comments

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from common2 import train
 import sys
 from contextlib import redirect_stdout
 
-def getNetwork(name):
+def get_network(name):
     if name == "ResNet50":
         return tf.keras.applications.ResNet50V2, tf.keras.applications.resnet_v2.preprocess_input
     elif name == "ResNet18":
@@ -16,36 +16,33 @@ def getNetwork(name):
     else:
         print("Network not found")
 
+def create_path(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print("Directory " , path ,  " Created")
+    else:
+        print("Directory " , path ,  " already exists")
 
 
 tf.keras.backend.set_image_data_format('channels_last')
-PATH = "samples"
+PATH = "samples" #Head path to dataset samples
 samples = [512,1024]
 
 networks_to_train = ["ResNet50", "InceptionV3"]
 
 for network_name in networks_to_train:
     SAVE_PATH = network_name
-    network, preprocess_input = getNetwork(network_name)
+    network, preprocess_input = get_network(network_name)
 
-    if not os.path.exists(SAVE_PATH):
-        os.mkdir(SAVE_PATH)
-        print("Directory " , SAVE_PATH ,  " Created")
-    else:
-        print("Directory " , SAVE_PATH ,  " already exists")
+    create_path(SAVE_PATH
 
     for sample in samples:
         SAVE_PATH_FOR_SAMPLE = SAVE_PATH + "/{}".format(sample)
 
-        if not os.path.exists(SAVE_PATH_FOR_SAMPLE):
-            os.mkdir(SAVE_PATH_FOR_SAMPLE)
-            print("Directory " , SAVE_PATH_FOR_SAMPLE ,  " Created")
-        else:
-            print("Directory " , SAVE_PATH_FOR_SAMPLE,  " already exists")
+        create_path(SAVE_PATH_FOR_SAMPLE)
 
-        #Redirect stdout to file for the training
+        #Redirect stdout to file to save training data
         with open(SAVE_PATH_FOR_SAMPLE + "/log.log", 'w') as f, redirect_stdout(f):
-            #sys.stdout = open(SAVE_PATH_FOR_SAMPLE + "/log.log", 'w')
             train(
                 network = network,
                 preprocess_input = preprocess_input,
