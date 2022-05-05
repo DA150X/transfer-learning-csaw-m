7e15a754a34d219bb89a3f92a2663f37f3aafb7d
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2022-05-05 10:09:33 +0200

    fix the trendline

--- plots/sample_size_scatter.py
+++ plots/sample_size_scatter.py
@@ -52,6 +52,8 @@ def create_chart_for_metric(metric, args):
         'ResNet50V2': '#ffa600',
     }
 
+    xvals = [100, 500, 1000, 3000, 5000, 7000, 9523]
+
     for label in labels:
         plt.figure(figsize=(14, 10))
         fig, ax = plt.subplots(figsize=(14, 10))
@@ -59,7 +61,7 @@ def create_chart_for_metric(metric, args):
         plt.title(make_title(metric, label))
         plt.ylabel(make_y_axis_label(metric, label))
         plt.xlabel('Sample size')
-        plt.xticks([0, 1, 2, 3, 4, 5, 6], ['100', '500', '1000', '3000', '5000', '7000', '9523'])
+        plt.xticks([100, 3000, 5000, 7000, 9523], ['100', '3000', '5000', '7000', '9523'])
 
         x_all = []
         y_all = []
@@ -71,10 +73,14 @@ def create_chart_for_metric(metric, args):
             for sample_size in sample_sizes:
                 values = get_test_results_for_label_network_and_sample_size(args.path_to_csv, metric, label, network, sample_size)
                 for scale_factor, value in values.items():
+                    if label != 'If_cancer':
+                        if scale_factor != '1':
+                            print(network, label, sample_size, scale_factor)
+                            continue
                     scale_factor = int(scale_factor)
-                    x.append(inc)
+                    x.append(xvals[inc])
                     y.append(value)
-                    x_all.append(inc)
+                    x_all.append(xvals[inc])
                     y_all.append(float(value))
                     z.append(100 + (350 * scale_factor))
                 inc += 1
@@ -101,7 +107,7 @@ def create_chart_for_metric(metric, args):
         )
 
         plt.ylim([min(plt.ylim()) - 0.2 * max(plt.ylim()), max(plt.ylim()) + 0.2 * max(plt.ylim())])
-        plt.xlim([min(plt.xlim()) - 0.2 * max(plt.xlim()), max(plt.xlim()) + 0.2 * max(plt.xlim())])
+        # plt.xlim([min(plt.xlim()) - 0.2 * max(plt.xlim()), max(plt.xlim()) + 0.2 * max(plt.xlim())])
         plt.tight_layout()
 
         filename = f'{label}_{metric}'
